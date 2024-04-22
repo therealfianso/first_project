c35bb45b10675c2d2f4efca5322a31e0e877ed51
therealfianso <125743052+therealfianso@users.noreply.github.com>
2024-04-22 10:41:00 +0100

    adding project

--- nombre divisible.py
+++
@@ -1,9 +0,0 @@
-def nombreDivisibles(L,n):
-    div = 0
-    for k in L:
-        if (k%n == 0):
-            div = div + 1
-    return div
-L=[5,9,3,5,6,7,87,4]
-n=3
-print("Le nombre d'élément de L qui sont divisible par " , n , " est : " ,nombreDivisibles(L,n))
